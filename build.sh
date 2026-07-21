7aecfb26c88ff4b96e1e9c46d36a8b12c0a712ce
Gabryel Marques <gabryelmarques17@gmail.com>
2026-07-20 22:46:10 -0300

    Adiciona script para criar meu superusuario

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Cria o SEU usuário admin automaticamente se não existir
+python manage.py createsuperuser --noinput --username gabryel --email gabryelmarques17@gmail.com || true
